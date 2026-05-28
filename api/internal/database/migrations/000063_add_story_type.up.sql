b23d06300e3d549be7b198b2bd9032dee14a8ae2
watchtower <watchtower@local>
2026-05-28 20:53:58 +0000

    fix: add story to work_items_type_check DB constraint

---
+++ api/internal/database/migrations/000063_add_story_type.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE work_items DROP CONSTRAINT work_items_type_check;
+ALTER TABLE work_items ADD CONSTRAINT work_items_type_check CHECK (type IN ('task', 'ticket', 'bug', 'feedback', 'epic', 'story'));
